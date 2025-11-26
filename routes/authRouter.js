7206b2857ddeb8e8558bba6d66cf41510497f6b9
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2025-11-26 09:14:23 +0530

    handler fix

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { registerHandler } = require('../controller/authController');
+const { registerHandler, loginHandler, getme, logoutHandler } = require('../controller/authController');
 
 const authRouter = express.Router();
 
-// authRouter.route("/login").post();
+authRouter.route("/login").post(loginHandler);
 authRouter.route("/register").post(registerHandler);
-// authRouter.route("/getme").get();
+authRouter.route("/getme").get(getme);
 
-// authRouter.route("/logout").post();
+authRouter.route("/logout").post(logoutHandler);
 
 module.exports = authRouter;
